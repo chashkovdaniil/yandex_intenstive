8861dd6b41d95e82e5de46cf62c72c420ac86001
nvxden <clever411@yandex.ru>
2021-12-09 00:57:56 +0300

    Add SymptomCheckNamedIconTray

--- lib/modules/education/symptom_check.dart
+++ lib/modules/education/symptom_check.dart
@@ -22,9 +22,51 @@ class SymptomCheck extends StatelessWidget
               'Check you symptoms for',
               style: Theme.of(context).textTheme.caption!.copyWith(fontSize: 22.0),
             ),
+            SymptomCheckNamedIconTray(),
           ],
         ),
       )
+    );
+  }
+}
+
+
+
+class NamedIcon extends StatelessWidget
+{
+  final String _title;
+  final String _icon;
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Row(
+      children: [
+        Image.asset(_icon, width: 30.0, height: 30.0),
+        const SizedBox(width: 7.0),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
+          child: Text(_title, style: Theme.of(context).textTheme.headline5),
+        ),
+      ],
+    );
+  }
+
+  const NamedIcon(this._title, this._icon);
+}
+
+class SymptomCheckNamedIconTray extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: const [
+        NamedIcon('Cold', 'assets/images/cold@3x.png'),
+        NamedIcon('Flu',  'assets/images/flu@3x.png'),
+        NamedIcon('Cold', 'assets/images/covid@3x.png'),
+      ],
     );
   }
 }

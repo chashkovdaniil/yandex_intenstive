c97a96b1a71b669da0e76093eed7ce604d139ae4
nvxden <clever411@yandex.ru>
2021-12-08 17:08:10 +0300

    Add methods in abstruct class CovidDatasource

--- lib/core/data/datasources/covid_datasource.dart
+++ lib/core/data/datasources/covid_datasource.dart
@@ -1,6 +1,23 @@
-abstract class CovidDatasource {
-  Future<String> summary();
-  Future<String> countries();
-  Future<String> dayOneCountry(String country);
-  Future<String> dayOneSummary();
+/*
+ * Абстрактный класс, с помощью которого можно получить данные по ковиду
+ */
+
+import 'package:yandex_intensive/core/domain/entities/covid.dart';
+
+
+
+abstract class CovidDatasource
+{
+  Future<CovidReport> operator()([
+    DateTime date     = DateTime.now(),
+    String?  country  = null, // null — весь мир
+    String?  province = null, // null — вся страна
+  ]) async;
+
+  Future<List<String>> countryCodes() async;
+  Future<String>       countryName(String code) async;
 }
+
+
+
+// END

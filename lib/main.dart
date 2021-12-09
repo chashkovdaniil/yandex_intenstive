458e9dbca7696e51325cb74df1dd489a7c5654e8
nvxden <clever411@yandex.ru>
2021-12-09 19:52:57 +0300

    Move symptom_check in presentation directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yandex_intensive/modules/home/presentation/home_screen.dart';
-import 'package:yandex_intensive/modules/education/symptom_check.dart';
+import 'package:yandex_intensive/modules/education/presentation/symptom_check.dart';
 
 void main() {
   runApp(MyApp());

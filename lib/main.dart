6fc28e82366d909935d5f795abcf9debc9483c0f
nvxden <clever411@yandex.ru>
2021-12-10 15:14:51 +0300

    Add sprintf in yaml, comment Provider (don't work)

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'modules/home/presentation/home_screen.dart';
 
 void main() {
-  runApp(ProviderScope(
-    child: MyApp(),
-  ));
+  runApp(MyApp());
+  // runApp(ProviderScope(
+  //   child: MyApp(),
+  // ));
 }
 
 class MyApp extends StatelessWidget {
@@ -13,12 +14,13 @@ class MyApp extends StatelessWidget {
   @override
   // ignore: prefer_expression_function_bodies
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Yandex Intensive Covid',
-      initialRoute: HomeScreen.route,
-      routes: {
-        HomeScreen.route: (_) => HomeScreen(),
-      },
+      home: Scaffold(body: Text("Hello")),
+      // initialRoute: HomeScreen.route,
+      // routes: {
+      //   HomeScreen.route: (_) => HomeScreen(),
+      // },
     );
   }
 }

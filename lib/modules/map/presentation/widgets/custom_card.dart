c1f234dfa4c0c52063ccd9ceef88825e00f94599
doldrums <doldrums.317@gmail.com>
2021-12-11 11:54:53 +0300

    custom card edits

--- lib/modules/map/presentation/widgets/custom_card.dart
+++ lib/modules/map/presentation/widgets/custom_card.dart
@@ -3,23 +3,25 @@ import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../configs/colors.dart';
+
 /// Something wrong is here
 /// in progress
 class CurvePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     var paint = Paint()
-      ..color = Colors.green
+      ..color = grey
       ..style = PaintingStyle.stroke
       ..strokeWidth = 2.0;
 
     var r = size.height * 0.3;
-    var R = size.height * 1.4;
+    var R = size.height * 1.3;
     var path = Path()
       ..lineTo(0, size.height - r)
       ..quadraticBezierTo(0 + r - sqrt(2) / 2 * r,
           size.height - r + sqrt(2) / 2 * r, r, size.height)
-    // lower semicircle
+      // lower semicircle
       // instead of r use can use another koeff
       ..lineTo(R + r - sqrt(R * R - size.height * size.height), size.height)
       ..quadraticBezierTo(r + R, R + r,
--- lib/modules/map/presentation/widgets/top_country_card.dart
+++ lib/modules/map/presentation/widgets/top_country_card.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
 
 import '../../../../configs/colors.dart';
-import '../../../../configs/constants.dart';
-import '../../../../configs/text_styles.dart';
-import '../../../general/navigation_views.dart';
 import '../screens/country_details_screen.dart';
+import 'custom_card.dart';
 import 'on_tap_opacity.dart';
 
 class TopCountryCard extends StatefulWidget {
@@ -32,113 +29,110 @@ class TopCountryCard extends StatefulWidget {
 class _TopCountryCardState extends State<TopCountryCard> {
   @override
   Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.only(top: 16),
+    padding: const EdgeInsets.only(top: 56),
         child: OnTapOpacityContainer(
           onTap: () => Navigator.of(context).push(CountryDetailsScreen.route()),
           child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: 80,
-            decoration: BoxDecoration(
-              color: white,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(
-                color: grey.withOpacity(0.3),
-              ),
+            color: white,
+            width: 400,
+            height: 30,
+            child: CustomPaint(
+              painter: CurvePainter(),
             ),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 12, right: 6),
-                  child: CircularPercentIndicator(
-                    radius: 60.0,
-                    animateFromLastPercent: true,
-                    animation: true,
-                    animationDuration: 800,
-                    circularStrokeCap: CircularStrokeCap.round,
-                    lineWidth: 7.0,
-                    backgroundColor: secondary,
-                    percent: widget.countryProgress,
-                    center: Text(
-                      '${(widget.countryProgress * 100).toStringAsFixed(0)}%',
-                    ),
-                    progressColor: widget.countryProgressColor,
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding:
-                            const EdgeInsets.only(top: 10, left: 12, bottom: 6),
-                        child: Text(
-                          widget.countryTitle,
-                          textAlign: TextAlign.start,
-                          style: TextStyles.titleCountry,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 12),
-                        child: SizedBox(
-                          width: 150,
-                          child: Text(
-                            StringValues.mapTopAffected + widget.affectedValue,
-                            textAlign: TextAlign.start,
-                            style: TextStyles.infoCountry,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 12, bottom: 4.0),
-                        child: SizedBox(
-                          width: 150,
-                          child: Text(
-                            StringValues.mapTopRecovered +
-                                widget.recoveredValue,
-                            textAlign: TextAlign.start,
-                            style: TextStyles.infoCountry,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const Spacer(),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 12, right: 12.0),
-                    child: OnTapOpacityContainer(
-                      //TODO: what this button is for lmao ??
-                      onTap: () => showAlertDialog(
-                        context: context,
-                        title: 'Enable notification',
-                        content:
-                            'Enable notification so you can always be up to date with new information.',
-                        cancelActionText: 'Later',
-                        defaultActionText: 'Enable now',
-                      ),
-                      child: IconButton(
-                        icon: const Icon(
-                          CupertinoIcons.bell,
-                          color: grey,
-                          size: 20,
-                        ),
-                        onPressed: () => showAlertDialog(
-                          context: context,
-                          title: 'Enable notification',
-                          content:
-                              'Enable notification so you can always be up to date with new information.',
-                          cancelActionText: 'Later',
-                          defaultActionText: 'Enable now',
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            // child: Row(
+            //   children: [
+            //     Padding(
+            //       padding: const EdgeInsets.only(left: 12, right: 6),
+            //       child: CircularPercentIndicator(
+            //         radius: 60.0,
+            //         animateFromLastPercent: true,
+            //         animation: true,
+            //         animationDuration: 800,
+            //         circularStrokeCap: CircularStrokeCap.round,
+            //         lineWidth: 7.0,
+            //         backgroundColor: secondary,
+            //         percent: widget.countryProgress,
+            //         center: Text(
+            //           '${(widget.countryProgress * 100).toStringAsFixed(0)}%',
+            //         ),
+            //         progressColor: widget.countryProgressColor,
+            //       ),
+            //     ),
+            //     Align(
+            //       alignment: Alignment.topLeft,
+            //       child: Column(
+            //         crossAxisAlignment: CrossAxisAlignment.start,
+            //         children: [
+            //           Padding(
+            //             padding:
+            //                 const EdgeInsets.only(top: 10, left: 12, bottom: 6),
+            //             child: Text(
+            //               widget.countryTitle,
+            //               textAlign: TextAlign.start,
+            //               style: TextStyles.titleCountry,
+            //             ),
+            //           ),
+            //           Padding(
+            //             padding: const EdgeInsets.only(left: 12),
+            //             child: SizedBox(
+            //               width: 150,
+            //               child: Text(
+            //                 StringValues.mapTopAffected + widget.affectedValue,
+            //                 textAlign: TextAlign.start,
+            //                 style: TextStyles.infoCountry,
+            //               ),
+            //             ),
+            //           ),
+            //           Padding(
+            //             padding: const EdgeInsets.only(left: 12, bottom: 4.0),
+            //             child: SizedBox(
+            //               width: 150,
+            //               child: Text(
+            //                 StringValues.mapTopRecovered +
+            //                     widget.recoveredValue,
+            //                 textAlign: TextAlign.start,
+            //                 style: TextStyles.infoCountry,
+            //               ),
+            //             ),
+            //           ),
+            //         ],
+            //       ),
+            //     ),
+            //     const Spacer(),
+            //     Align(
+            //       alignment: Alignment.topLeft,
+            //       child: Padding(
+            //         padding: const EdgeInsets.only(top: 12, right: 12.0),
+            //         child: OnTapOpacityContainer(
+            //           //TODO: what this button is for lmao ??
+            //           onTap: () => showAlertDialog(
+            //             context: context,
+            //             title: 'Enable notification',
+            //             content:
+            //                 'Enable notification so you can always be up to date with new information.',
+            //             cancelActionText: 'Later',
+            //             defaultActionText: 'Enable now',
+            //           ),
+            //           child: IconButton(
+            //             icon: const Icon(
+            //               CupertinoIcons.bell,
+            //               color: grey,
+            //               size: 20,
+            //             ),
+            //             onPressed: () => showAlertDialog(
+            //               context: context,
+            //               title: 'Enable notification',
+            //               content:
+            //                   'Enable notification so you can always be up to date with new information.',
+            //               cancelActionText: 'Later',
+            //               defaultActionText: 'Enable now',
+            //             ),
+            //           ),
+            //         ),
+            //       ),
+            //     ),
+            //   ],
+            // ),
           ),
         ),
       );

d637ea608eb4a7bce898154e405e3780277cc10f
nvxden <clever411@yandex.ru>
2021-12-13 10:28:03 +0300

    Add colors and constants from other branch

---
+++ lib/configs/colors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  // basic colors
+  static const Color primary = Color(0xFFFF647C);
+  static const Color secondary = Color(0xFFE4E4E4);
+  static const Color black = Color(0xFF000000);
+  static const Color grey = Color(0xFFE4E4E4);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color red = Color(0xffFF647C);
+
+// Colors for progress bars
+  static const Color green = Color(0xFF00C48C);
+  static const Color blue = Color(0xFF6979F8);
+  static const Color orange = Color(0xffF0CA01);
+
+// Colors for map gradient
+  static const Color affected1 = Color(0xFFFFF9FA);
+  static const Color affected2 = Color(0xFFFFCBD3);
+  static const Color affected3 = Color(0xFFFF9DAC);
+  static const Color affected4 = Color(0xFFFF7489);
+  static const Color affected5 = Color(0xFFFF4F6A);
+  static const Color affected6 = Color(0xFFFF2E4E);
+
+// Colors for education screen
+  static const Color educationGray = Color(0xFF999999);
+  static const Color educationBlackSubtitle = Color(0xFF151522);
+  static const Color educationBlackHeader = Color(0xFF171725);
+}
---
+++ lib/configs/constants.dart
@@ -0,0 +1,55 @@
+import 'colors.dart';
+
+class StringValues {
+  static String get mapTitle => 'World Wide map';
+
+  static String get mapAreas => 'COVID - 19 Affected Areas';
+
+  static String get mapAreasLess => 'Most Affected';
+
+  static String get mapAreasMost => 'Less Affected';
+
+  static String get mapTop => 'Top Countries';
+
+  static String get mapTopAffected => 'Affected - ';
+
+  static String get mapTopRecovered => 'Recovered - ';
+
+  static String get mapRest => 'Rest of the world';
+
+  static String get mapRestDescription => 'List of all Affected Countries';
+
+  static String get mapRestCases => ' - Death cases reported';
+
+  static String get countryStatisticsTitle => ' - statistic';
+
+  static String get countryStatisticsInfectedCases =>
+      ' Infected cases reported';
+
+  static String get countryStatisticsDeathCases => ' Death cases reported';
+
+  static String get countryStatisticsGraphTitle =>
+      'Current affected and death people';
+
+  static String get countryStatisticsDeath => 'Death';
+
+  static String get countryStatisticsAffected => 'Affected';
+
+  // Strings for education
+  static const String diagnosisDescription =
+      'If you feel some symptoms or you are thinking that you may'
+      'be infected. Please report your self so other people can'
+      'be aware that someone who is in isolation is nearby them.';
+
+  static const String diagnosisWarningCaps = 'COVSTATS';
+
+  static const String diagnosisWarning =
+      ' gives everyone the opportunity to report their symptoms'
+      'anonymously for the benefit of others';
+
+  static const String diagnosisHeader = 'Report my Status';
+
+  static const String diagnosisButtonText = 'Please report your status';
+
+  static const String diagnosisRussianPhoneNumber = '88002000112';
+}

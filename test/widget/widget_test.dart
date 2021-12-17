2e2fddc5c007a913d524174e4b9e35bb9dfa1575
Daniil <chashkovdaniil@gmail.com>
2021-12-17 19:05:16 +0500

    fix warnings

--- test/widget/widget_test.dart
+++ test/widget/widget_test.dart
@@ -6,22 +6,4 @@
 // // tree, read text, and verify that the values of widget properties are correct.
 
 // void main() async {
-//   // var _nds = await CovidNetworkDatasource().countries();
-//   // print(_nds);
-//   // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//   //   // Build our app and trigger a frame.
-//   //   await tester.pumpWidget(MyApp());
-
-//   //   // Verify that our counter starts at 0.
-//   //   expect(find.text('0'), findsOneWidget);
-//   //   expect(find.text('1'), findsNothing);
-
-//   //   // Tap the '+' icon and trigger a frame.
-//   //   await tester.tap(find.byIcon(Icons.add));
-//   //   await tester.pump();
-
-//   //   // Verify that our counter has incremented.
-//   //   expect(find.text('0'), findsNothing);
-//   //   expect(find.text('1'), findsOneWidget);
-//   // });
 // }

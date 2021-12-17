a4b5e4c4dc2945206e5d55b33816d9113cc827d5
Daniil <chashkovdaniil@gmail.com>
2021-12-17 19:09:34 +0500

    fix warnings

--- test/widget/map_test.dart
+++ test/widget/map_test.dart
@@ -2,7 +2,7 @@
 // // import 'package:flutter_test/flutter_test.dart';
 // // import 'package:yandex_intensive/modules/map/presentation/screens/map_screen.dart';
 
-// void main() {
+void main() {
 //   // group('Map Page Widget Tests', () {
 //   //   testWidgets('Testing Scrolling', (tester) async {
 //   //     await tester.pumpWidget(const MapScreen());
@@ -26,4 +26,4 @@
 //   //     );
 //   //   });
 //   // });
-// }
+}
